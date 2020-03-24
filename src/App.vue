6fbe02b78bba4aeea816fa8dedd46c93702e066d
czj6 <673330108@qq.com>
2020-03-24 15:48:38 +0800

    样式基本框架

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <!-- <el-container>
+    <el-aside>这是侧边导航</el-aside>
+    <el-container>这是容器</el-container>
+
+  </el-container> -->
+
+  <el-container style="height: 500px; border: 1px solid #eee">
+  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <i class="fa fa-qq fa-2x icon1" aria-hidden="true"></i>
+      <router-link to="/home"><i class="fa fa-comment-o fa-2x icon2" aria-hidden="true"></i></router-link>
+      <i class="fa fa-user fa-2x icon3" aria-hidden="true"></i>
+      <i class="fa fa-cog fa-2x icon4" aria-hidden="true"></i>
+  </el-aside>
+  <transition>
+    <router-view></router-view>
+  </transition>
+
+
+</el-container>
+
+</template>
+<script>
+import "../static/font-awesome/css/font-awesome.min.css"
+export default {
+   data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        tableData: Array(20).fill(item),
+
+      }
+    },
+    methods:{
+
+    }
+}
+</script>
+<style scoped>
+.el-aside{
+  position: relative;
+}
+.el-aside .icon1{
+  position: absolute;
+  left: 39%;
+  top: 5%;
+}
+.el-aside .icon2{
+  position: absolute;
+  top: 40%;
+  left: 39%;
+}
+.el-aside .icon3{
+  position: absolute;
+  top: 55%;
+  left: 39%;
+}
+.el-aside .icon4{
+  position: absolute;
+  top: 90%;
+  left: 39%;
+}
+  .el-aside {
+    color: #333;
+  }
+</style>
---
+++ src/components/chat/chat.vue
@@ -0,0 +1,125 @@
+<template>
+  <el-container>
+      <el-container>
+    <el-header style="font-size: 12px">
+      <span class="group_introduction_container">
+        <i class="fa fa-superpowers fa-2x" aria-hidden="true"></i>
+        <p class="group_introduction">
+          <span>bootstrap</span>
+          <span></span>
+        </p>
+      </span>
+      <span class="header_icons">
+        <i class="fa fa-search fa-1x" aria-hidden="true"></i>
+        <i class="fa fa-share fa-1x" aria-hidden="true"></i>
+        <!-- <i class="fa fa-chevron-circle-down fa-1x" aria-hidden="true"></i> -->
+        <el-dropdown trigger="click"  @command="handleCommand">
+        <i class="fa fa-chevron-circle-down fa-1x" aria-hidden="true"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item command="c">Mute   <i class="fa fa-sliders" aria-hidden="true"></i></el-dropdown-item>
+          <el-dropdown-item>Delete   <i class="fa fa-trash-o" aria-hidden="true"></i></el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      </span>
+    </el-header>
+
+    <el-main>
+    </el-main>
+    <el-footer>
+      <el-input type="textarea" autosize placeholder="请输入内容" v-model="textarea">
+      </el-input>
+      <el-button icon="el-icon-s-promotion" circle></el-button>
+    </el-footer>
+  </el-container>
+  <el-container class="group_info" v-show="flag">
+    <el-header>
+      <i class="fa fa-angle-left fa-2x" aria-hidden="true" @click="flag=!flag"></i>
+    </el-header>
+    <el-main>
+        <i class="fa fa-superpowers fa-2x" aria-hidden="true"></i>
+        <h5>bootstrap</h5>
+        <p>html,js,css and so on</p>
+    </el-main>
+    <el-footer>
+      photo
+      <el-card shadow="always">
+
+      </el-card>
+      name
+      <el-card shadow="always">
+
+      </el-card>
+      topic
+      <el-card shadow="always">
+
+      </el-card>
+    </el-footer>
+  </el-container>
+  </el-container>
+
+</template>
+<script>
+export default {
+    data(){
+      return {
+          flag:false,
+          textarea:''
+      }
+    },
+    methods:{
+      changeFlag(){
+        this.flag=!this.flag;
+      },
+      handleCommand(command) {
+        // this.$message('click on item ' + command);
+        this.flag = !this.flag
+      }
+    }
+}
+</script>
+<style scoped>
+.el-header {
+  display: flex;
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+    justify-content: space-between;
+  }
+.el-header .group_introduction{
+  display: flex;
+  flex-direction: column;
+  padding: 4px;
+}
+.el-header .group_introduction_container{
+  display: flex;
+  justify-content: space-between;
+  width: 15%;
+  align-items: center;
+}
+.el-header .header_icons{
+  display: flex;
+  width: 15%;
+  justify-content: space-around;
+  align-items: center;
+}
+
+
+.group_info{
+  /* background-color: pink; */
+  width: 19%;
+
+}
+.group_info .el-header{
+  background-color: #fff;
+  align-items: center;
+}
+.group_info .el-main{
+  flex: 0.7;
+}
+.group_info .el-footer{
+  flex: 1;
+}
+.el-textarea{
+  width: 70%;
+}
+</style>
